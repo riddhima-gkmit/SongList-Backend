3ade777f93270a7761f41ff8de330ce8005a2614
riddhima-gkmit <riddhima@gkmit.co>
2026-01-07 00:36:59 +0530

    feat(music): add serializers for song creation and retrieval

---
+++ music/serializers/__init__.py
@@ -0,0 +1,10 @@
+from .song import SongSerializer
+from .playlist import PlaylistSerializer, PlaylistSongAddSerializer
+from .review import SongReviewSerializer
+
+__all__ = [
+    "SongSerializer",
+    "PlaylistSerializer",
+    "PlaylistSongAddSerializer",
+    "SongReviewSerializer",
+]
