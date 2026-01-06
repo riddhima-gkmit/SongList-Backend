ee71c29a36bd83dd4b7bfaec3a58bd93772d8f6b
riddhima-gkmit <riddhima@gkmit.co>
2026-01-06 22:58:09 +0530

    feat(common): add data seeding management commands

---
+++ common/management/commands/seed_admins.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+from users.models import User
+from common.enums import UserRole
+
+class Command(BaseCommand):
+    help = 'Seeds the database with 1 admin user'
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        username = "admin"
+        email = "admin@example.com"
+        password = "password123"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_user(
+                username=username,
+                email=email,
+                password=password,
+                first_name="Admin",
+                last_name="User",
+                role=UserRole.ADMIN
+            )
+            self.stdout.write(self.style.SUCCESS(f'Successfully created admin: {username}'))
+        else:
+            self.stdout.write(self.style.WARNING(f'Admin {username} already exists.'))
